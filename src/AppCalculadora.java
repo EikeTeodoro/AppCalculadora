436450d6e264771aaaf2454612d74a69f7ec4e2a
EikeTeodoro <eike_silva@estudante.sesisenai.org.br>
2025-08-27 16:22:45 -0300

    Dados para executar a calculadora
    Com essa alteração, o código irá executar como uma calculadora com valores definidos.

--- src/AppCalculadora.java
+++ src/AppCalculadora.java
@@ -1,6 +1,8 @@
 public class AppCalculadora {
     public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Soma: " + Calculadora.soma(10, 5));
         System.out.println("Subtração: " + Calculadora.subtracao(10, 5));
+        System.out.println("Divisão: " + Calculadora.divisao(10, 5));
     }
 }
